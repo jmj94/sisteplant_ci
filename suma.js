1d397ff25579c34f13c16a65e536c921a9acceba
JMaraver <JMaraver@sisteplant.com>
2021-05-27 11:08:24 +0200

    Primera subida al repositorio de integracion continua

---
+++ suma.js
@@ -0,0 +1,6 @@
+function suma(a, b) {
+    console.log("Vas a ejectuar una suma");
+    return parseInt(a) + parseInt(b);
+}
+
+module.exports = suma;
---
+++ suma.test.js
@@ -0,0 +1,9 @@
+const suma = require('./suma');
+
+test('2 + 5 = 7', () => {
+    expect(suma(2, 5)).toBe(7); // Aserción propia de jest
+});
+
+test('a + 5 = NaN', () => {
+    expect(suma('a', 5)).toBe(7);
+});
